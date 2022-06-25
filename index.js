562d68c23c9f15bf765c77d68ca424e5ca0ba9ab
Rahul <rahullsoni04@gmail.com>
2022-06-25 12:43:18 +0530

    v16 creating arguments using yargs

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const chalk=require("chalk");
-console.log(chalk.underline.blue("helllo"))
+const yargs=require("yargs")
+yargs.command({
+    command:"add",
+    description:"this will add values",
+    handle:function(){
+        console.log("Add a note");
+    }
+})
+yargs.command({
+    command:"remove",
+    description:"this will remove values",
+    handle:function(){
+        console.log("Remove a note");
+    }
+})
+yargs.command({
+    command:"list",
+    description:"this will list data",
+    handle:function(){
+        console.log("list data");
+    }
+})
+yargs.command({
+    command:"read",
+    description:"this will dsiplay all data",
+    handle:function(){
+        console.log("Display data");
+    }
+})
+
+console.log(yargs.argv)
