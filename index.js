6e88ad046e4ba6df6e454fd72d6539eb975f6bb7
Rahul <rahullsoni04@gmail.com>
2022-06-26 22:00:25 +0530

    v34 hadling error

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const request = require('postman-request');
 const url=require("./config.js").wheather;
 const baseUsrl=url.Base_url+url.API_KEY+"&q=London";
 
-const mapboxUrl= require("./config.js").mapbox;
-const mapboxBaseUsrl=mapboxUrl.Base_url+"/Los%20Angeles.json"+mapboxUrl.Access_token+"&limit=1";
-
-request({url:baseUsrl,json:true},async (error, response, body)=>{
-    await console.log("It is currently "+response.body.current.temp_c+" It feels like "+response.body.current.feelslike_c)
+request({url:baseUsrl,json:true},(error, response, body)=>{
+    if(error){
+        console.log("Unable to connect ")
+    }else if(response.body.error){
+        console.log("Unable to fetch data "+response.error)
+    }else{
+        console.log("It is currently "+response.body.current.temp_c+" It feels like "+response.body.current.feelslike_c)
+    }
 })
 
+const mapboxUrl= require("./config.js").mapbox;
+const mapboxBaseUsrl=mapboxUrl.Base_url+"/Los%20Angeles.json"+mapboxUrl.Access_token+"&limit=1";
+console.log(mapboxBaseUsrl)
 request({url:mapboxBaseUsrl,json:true},(error, response, body)=>{
-    const path=response.body.features[0].center;
-    console.log("Longitude "+path[0]+" latitude "+path[1])
+    if(error){
+        console.log("Unable to connect ")
+    }else if(response.body.message || response.body.features.length === 0 ){
+        console.log("Unable to fetch data \n Reason: "+response.body.message)
+    }else{
+        const path=response.body.features[0].center;
+        console.log("Longitude "+path[0]+" latitude "+path[1]) 
+    }
 })
 
